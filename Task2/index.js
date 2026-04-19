2f86bd9f16dd9e300752d245207f02bae6d08cb2
srihema2005 <mandepudisrihema21@gmail.com>
2026-04-19 15:23:41 +0530

    Update index.js

--- Task2/index.js
+++ Task2/index.js
@@ -1,1 +1,43 @@
 
+'use strict';
+
+const fs = require('fs');
+const EventEmitter = require('events');
+const KEY_CODES = require('./keycodes');
+
+const EVENT_SIZE = 24;       // Linux input event size
+const EV_KEY = 1;
+const EVENT_TYPES = ['keyup', 'keypress', 'keydown'];
+
+class Keyboard extends EventEmitter {
+  constructor(device = 'event0') {
+    super();
+    this.device = device;
+
+    this.stream = fs.createReadStream(`/dev/input/${device}`);
+    this.stream.on('data', this.handleData.bind(this));
+    this.stream.on('error', err => this.emit('error', err));
+  }
+
+  handleData(data) {
+    for (let i = 0; i + EVENT_SIZE <= data.length; i += EVENT_SIZE) {
+      const buffer = data.slice(i, i + EVENT_SIZE);
+
+      if (buffer.readUInt16LE(16) !== EV_KEY) continue;
+
+      const event = {
+        timeS: buffer.readUInt32LE(0),
+        timeUS: buffer.readUInt32LE(8),
+        keyCode: buffer.readUInt16LE(18),
+        keyId: KEY_CODES[buffer.readUInt16LE(18)],
+        type: EVENT_TYPES[buffer.readUInt32LE(20)],
+        device: this.device
+      };
+
+      this.emit(event.type, event);
+    }
+  }
+}
+
+Keyboard.Keys = KEY_CODES;
+module.exports = Keyboard;
